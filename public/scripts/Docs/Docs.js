d16ae0431875812e79e021d2a6283f72551a98dc
MEHLMAN <mailedouardmehlman@gmail.com>
2016-03-29 22:51:36 +0200

    Style fin

--- public/scripts/Docs/Docs.js
+++ public/scripts/Docs/Docs.js
@@ -1,24 +1,8 @@
 var Docs = React.createClass({
 	render:function(){
 		return(
+			<div className="container doc">
 			<article className="markdown-body">
-<h2><a id="user-content-github-markdown-css-demo" className="anchor" href="#github-markdown-css-demo" aria-hidden="true"><span className="octicon octicon-link"></span></a>GitHub Markdown CSS demo</h2>
-
-<p><a name="user-content-headers"></a></p><a name="user-content-headers">
-
-</a><h3><a name="user-content-headers"></a><a id="user-content-headers" className="anchor" href="#headers" aria-hidden="true"><span className="octicon octicon-link"></span></a>Headers</h3>
-
-<pre lang="no-highlight"><code># h2
-## h3
-### H3
-#### H4
-##### H5
-###### H6
-</code>
-</pre>
-
-Ce site a pour objectif de permettre aux personnes de poster des annonces d’objets qu’elles
- auraient perdus sur le plateau.
 
 <h1><a id="user-content-h1" className="anchor" href="#h1" aria-hidden="true"><span className="octicon octicon-link"></span></a>Documentation</h1>
 <h2><a id="user-content-h2" className="anchor" href="#h2" aria-hidden="true"><span className="octicon octicon-link"></span></a>Objectif du projet</h2>
@@ -112,8 +96,87 @@ On veut pouvoir récupérer la liste d’objet perdus avec toute les information
 On veut pouvoir récupérer la liste des lieux, ajouter un lieu, et supprimer un lieu. 
 </p>
 
+<h3><a id="user-content-alt-h3" className="anchor" href="#alt-h3" aria-hidden="true"><span className="octicon octicon-link"></span></a>Classe Helpers: </h3>
+<h5>FONCTION STATIQUE : </h5>
+<p>
+Pour chaque formulaire, on veut pouvoir filtrer ce qui a été soumis (vérifier en particulier que chaque champ a bien été rempli). 
+La fonction filter_input_array, utilisée avec INPUT_POST, et des callbacks adéquats, permet de rendre un tableau PHP avec les 
+mêmes clés que POST, mais avec des valeurs qui correspondent aux messages d’erreur adaptés.
+</p>
+<h2><a id="user-content-h2" className="anchor" href="#h2" aria-hidden="true"><span className="octicon octicon-link"></span></a>Le fichier server.php</h2>
+<h3><a id="user-content-alt-h3" className="anchor" href="#alt-h3" aria-hidden="true"><span className="octicon octicon-link"></span></a>.htaccess</h3>
+<p>Toutes les requêtes sont redirigées vers le fichier server.php. 
+La base est réécrite /MODAL/ correspondant au domaine d’adressage de l’application 
+(mais vous pouvez la modifier). Les fichiers php ne sont pas accessibles en statique par l’utilisateur, 
+et ne sont pas desservis en statique (comme le fait Apache si on ne lui précise aucun restriction).</p>
+<h3><a id="user-content-alt-h3" className="anchor" href="#alt-h3" aria-hidden="true"><span className="octicon octicon-link"></span></a>Le fichier server.php</h3>
+<p>Contrairement à ce qui a été suggéré dans le TD2 du MODAL web, nous avons fait le choix 
+de prendre le modèle de « contrôleur » pour le fichier central de notre application, server.php. 
+Ce fichier pourrait être considéré comme un équivalent d’index.php. 
+À l’aide d’un fichier .htaccess d’Apache, on redirige toutes les demandes vers 
+ce server.php, qui s’occupe de les traiter, comme le ferait un contrôleur standard dans une application web.</p><p>
+Nous tenons à prouver que, malgré cette décision personnelle, le projet est tout aussi bien structuré ainsi.</p><p>
+Toute requête au serveur, ou plus spécialement à la base de données, se caractérise par une URL, et une 
+méthode http qui la caractérise : GET, PUT, POST, DELETE – comme le veut le standard d’application en architecture REST.</p><p>
+On regroupe ainsi les requêtes similaires :</p><p>
+Connexion en GET, rendrait la vue correspondante, c’est à dire le formulaire de Connexion</p><p>
+Connexion en POST, correspondrait à l’envoi d’un formulaire, et donnant une réponse dépendant de la vérification des paramètres du formulaires (error ou result). </p><p>
+Connexion en PUT, correspondrait à une tentative de connexion à partir de l’identifiant de session, et retournerait l’objet JSON Utilisateur correspondant à l’utilisateur connecté, nom, prénom.</p><p>
+Pour la page d’erreur, à défaut de parcourir un tableau et de regarder si la page demandée en est une clé, on écrit à la fin du switch sur 
+les urls un default, qui retourne effectivement un page de redirection.</p><p>
+C’est une pratique de routage utilisée par de nombreux frameworks, dont nous avons voulu nous inspirer. </p>
+<h2><a id="user-content-h2" className="anchor" href="#h2" aria-hidden="true"><span className="octicon octicon-link"></span></a>LA VUE – JAVASCRIPT – REACTJS</h2>
+<h3><a id="user-content-alt-h3" className="anchor" href="#alt-h3" aria-hidden="true"><span className="octicon octicon-link"></span></a>MOTIVATION :</h3>
+<p>En vue de mon stage 2A d’informatique où je travaillerai en Javascript avec des outils qui m’ont déjà été spécifiés, 
+je désirais m’essayer à l’un d’entre eux, React. Je vous pris de ne pas voir cette initiative d’un mauvais œil, mais plutôt 
+de considérer ceci comme une tentative de prolongement du projet et de curiosité dans le domaine des applications WEB. Étant 
+donné la quantité de travail supplémentaire imposée par ce défi, je vous prie d’être indulgent envers cette décision.</p>
+<h3><a id="user-content-alt-h3" className="anchor" href="#alt-h3" aria-hidden="true"><span className="octicon octicon-link"></span></a>Qu’est-ce que React ?</h3>
+<p>React est l’API que Facebook utilise pour son application côté client. React permet de réaliser, ce que l’on appelle 
+la View dans le Model MVC. Ce n’est pas un framework, dans le sens où React ne donne aucune organisation particulière au 
+code. C’est simplement une librairie Javascript pour faire des interfaces utilisateurs.</p><p>
+React permet une abstraction du Document Object Model (le DOM), permettant une programmation plus simple, et des performances 
+accrues. Faire du Javascript sur des éléments virtuels Javascript qui représentent le DOM, est plus perfomant que de faire 
+des opérations directement sur le document Object Model.</p><p>
+React donne la possibilité de créer vos propres composants qui seront réutilisables plus tard et/ou peuvent être combinés. 
+Définir et manipuler ses composants devient très simple.</p><p>
+React utilise une syntaxe spéciale appelée JSX, proche de XML, et qui vous permet de mélanger en quelque sorte du HTML et du JavaScript. </p><p>
+Ce dernier critère est, de prime abord, effrayant, ces deux sphères étant traditionnellement séparées... Si vous regardez ce code sur Netbeans, par exemple, toutes les parties non logiques et 
+purement DOM virtuel seront soulignés (on n’aime pas les balises HTML en plein code Javascript).</p>
+<h3><a id="user-content-alt-h3" className="anchor" href="#alt-h3" aria-hidden="true"><span className="octicon octicon-link"></span></a>Dans notre projet...</h3>
+<h5>Sécurité</h5>
+<p>Avoir écrit toute la vue en Javascript, ne permet cependant pas à n’importe qui (en particulier quelqu’un de non connecté) d’avoir accès à nos données, ou de les modifier, car toute requête faite au serveur, 
+vérifie toujours l’identité de l’utilisateur côté PHP.</p>
+<h5>Transmission de données à partir de la Base</h5>
+<p>La quasi-totalité des informations transitant entre le navigateur et le serveur sont au format JSON (Javascript Object Notation), et sont interprétées de manière asynchrone grâce à des 
+procédures AJAX (AsynchronousJavascript And XML).</p>
+<h5>SPA (single page application)</h5>
+<p>On peut naviguer sur le site sans jamais recharger la page !</p>
+<h2><a id="user-content-h2" className="anchor" href="#h2" aria-hidden="true"><span className="octicon octicon-link"></span></a>Dossier public/scripts</h2>
+<p>Tous les fichiers écrit en ReactJS sont dans ce dossier.</p>
+<h3><a id="user-content-alt-h3" className="anchor" href="#alt-h3" aria-hidden="true"><span className="octicon octicon-link"></span></a>Dossier : public/scripts/Utilisateur</h3>
+<p>Pour commencer par un groupe simple de composants; prenons le composant Utilisateur, le composant UtilisateurEdit, et le 
+composant ListeUtilisateur. Le composant Utilisateur effectue juste un rendu de ses propriétés. Le composant UtilisateurEdit 
+rend un composant Utilisateur ainsi que deux boutons supplémentaires. Si ces boutons sont cliqués, ils appellent chacun une 
+des fonction qui leur est passé en propriété par Utilisateur. Le composant Utilisateur charge toutes les données utilisateurs, les 
+affiches dans un composant UtilisateurEdit, et possède aussi des fonctions qui correspondent à des appels asynchrones AJAX.</p>
+<h3><a id="user-content-alt-h3" className="anchor" href="#alt-h3" aria-hidden="true"><span className="octicon octicon-link"></span></a>Dossier : public/scripts/Lieu</h3>
+<p>Nous avons implémenté l’API GOOGLE MAPS dans LieuMap.js.</p>
+<h3><a id="user-content-alt-h3" className="anchor" href="#alt-h3" aria-hidden="true"><span className="octicon octicon-link"></span></a>Fichier : public/scripts/App/App.js</h3>
+<p>Fichier central de l’application React. Lors du chargement de la page, ce composant appelle la connexion, et charge les lieux. Son état se caractérise par l’onglet sélectionné. 
+Ce composant est rendu après l’appel du script de Google API, ce qui permet d’utiliser des Maps dans notre application.</p>
+<h3><a id="user-content-alt-h3" className="anchor" href="#alt-h3" aria-hidden="true"><span className="octicon octicon-link"></span></a>Fichier : public/scripts/Filter/FilterBar.js</h3>
+<p>Une fonctionnalité de filtre figure dans les pages lieux et des objets et permet une recherche instantanée des objets en fonction du nom. Ceci est réalisé à l’aide du changement
+d’état de la Liste (ListeAffiche ou ListeLieu). La value de l’input correspond en effet à l’état de la Liste. 
+Dès lors que l’état change, le composant doit donner un nouveau rendu : c’est le tableau des lieux filtré avec la fonction filterJavascript.</p>
+<h3><a id="user-content-alt-h3" className="anchor" href="#alt-h3" aria-hidden="true"><span className="octicon octicon-link"></span></a>Dossier : public/scripts/Affiche</h3>
+<p>Correspond au premier onglet de la barre de navigation ainsi que la page d’accueil du site. C’est là que tous les objets perdus sont affichés.
+ListeAffiche correspond au composant qui contient la FilterBar et la liste des Objets, et charge en asynchrone, toutes les données sur les objets depuis la BDD.
+Dans ObjetForm (formulaire pour déclarer la perte d’un objet), nous avons également implémenté une barre d’autocomplétion à la main, grâce à ReactJS.
+</p>
 
 		</article>
+		</div>
 		)
 	}
 });
